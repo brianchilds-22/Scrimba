4a2e54fe6238ffb85040ca4ba430a70f8dfcc49a
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-02-15 08:56:31 -0700

    first

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // SETTING THE STAGE
-let player = "Per";
-let opponent = "Nick";
-let game = "AmazingFighter";
-const points = 0;
-const hasWon = false;
+const player = "Per";
+const opponent = "Nick";
+const game = "AmazingFighter";
+let points = 0;
+let hasWon = false;
 
 // PLAYING THE GAME
 points += 100;
@@ -11,12 +11,29 @@ hasWon = true;
 
 // ANNOUNCING THE WINNER
 if (hasWon) {
-  console.log(
-    player + " got " + points + " points and won the " + game + " game!"
-  );
+  console.log(`
+    ${player} got ${points} points and won the ${game} game!"`);
 } else {
-  console.log("The winner is " + opponent + "! " + player + " lost the game");
+  console.log(`The winner is ${opponent}! ${player} lost the game`);
 }
 
 // Go through all variables and decide if they should be let or const
 // Change the console logs to use template strings instead of double quotes
+
+let myCourses = [
+  "Learn CSS Animations",
+  "UI Design Fundamentals",
+  "Intro to Clean Code",
+];
+
+function logItems(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+console.log(myCourses);
+
+// localStorage.setItem("myCredits", "100");
+
+let myCredits = localStorage.getItem("myCredits");
+console.log(myCredits);
